14ed5f3a9807f38d18c332e051880a71df14e158
erictuanle <33071161+erictuanle@users.noreply.github.com>
2017-11-22 03:54:38 +0300

    Update DoG.m

--- DoG.m
+++ DoG.m
@@ -1,11 +1,11 @@
 function w_so = DoG(v_so, n_oct, n_spo)
-%DoG calcule la Difference of Gaussians scale-space
+%DoG computes the difference of Gaussians scale-space
 %   ARGUMENTS:
-%   	v_so: scale space digital (o=1,...,n_oct et s=0,...,n_spo+2)
+%   	v_so: digital scale space (o=1,...,n_oct et s=0,...,n_spo+2)
 %   OUTPUT:
-%       w_so: DoG digital (o=1,...,n_oct et s=0,...,n_spo+2)
+%       w_so: digital DoG (o=1,...,n_oct et s=0,...,n_spo+2)
 
-%% Boucle de calcul
+%% Computation loops
 for o=1:n_oct
     for s=0:n_spo+1
         w_so{s+1,o+1} = v_so{s+2,o+1} - v_so{s+1,o+1};
